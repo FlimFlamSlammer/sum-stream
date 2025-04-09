7636d2865ec63568fbacff1569fb84947ae57066
Lionel Wijaya <lionel_cw@proton.me>
2025-04-09 16:58:53 +0700

    feat: add rules button to quickly view game rules

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -4,8 +4,15 @@ import { useGameContext } from "@/components/game-provider";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import CardDropDown from "@/components/ui/card-dropdown";
+import { Dialog, DialogFooter, DialogHeader } from "@/components/ui/dialog";
 import Display from "@/components/ui/display";
 import PlayingCard from "@/components/ui/playing-card";
+import {
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { useRef, useState } from "react";
 
 export default function Game() {
@@ -87,68 +94,88 @@ export default function Game() {
   }
 
   return (
-    <div className="min-h-screen flex flex-col items-center py-10 justify-between">
-      <div className="flex flex-col gap-3 items-center">
-        <h1 className="text-6xl font-bold mb-10">Sum Stream</h1>
-        <Display>{accumulator}</Display>
-        <div className="flex flex-row gap-2">
-          <CardDropDown
-            color="red"
-            onClick={(n) => onNumberClickHandler(n, colorToOperation.red)}
-          >
-            {colorToOperation.red(0, 0).str}
-          </CardDropDown>
-          <CardDropDown
-            color="yellow"
-            onClick={(n) => onNumberClickHandler(n, colorToOperation.yellow)}
-          >
-            {colorToOperation.yellow(0, 0).str}
-          </CardDropDown>
-          <CardDropDown
-            color="green"
-            onClick={(n) => onNumberClickHandler(n, colorToOperation.green)}
-          >
-            {colorToOperation.green(0, 0).str}
-          </CardDropDown>
-          <CardDropDown
-            color="blue"
-            onClick={(n) => onNumberClickHandler(n, colorToOperation.blue)}
-          >
-            {colorToOperation.blue(0, 0).str}
-          </CardDropDown>
-          <PlayingCard onClick={rotateColorToOperation}>↻</PlayingCard>
-          <PlayingCard
+    <>
+      <div className="min-h-screen flex flex-col items-center py-10 justify-between">
+        <div className="flex flex-col gap-3 items-center">
+          <h1 className="text-6xl font-bold mb-10">Sum Stream</h1>
+          <Display>{accumulator}</Display>
+          <div className="flex flex-row gap-2">
+            <CardDropDown
+              color="red"
+              onClick={(n) => onNumberClickHandler(n, colorToOperation.red)}
+            >
+              {colorToOperation.red(0, 0).str}
+            </CardDropDown>
+            <CardDropDown
+              color="yellow"
+              onClick={(n) => onNumberClickHandler(n, colorToOperation.yellow)}
+            >
+              {colorToOperation.yellow(0, 0).str}
+            </CardDropDown>
+            <CardDropDown
+              color="green"
+              onClick={(n) => onNumberClickHandler(n, colorToOperation.green)}
+            >
+              {colorToOperation.green(0, 0).str}
+            </CardDropDown>
+            <CardDropDown
+              color="blue"
+              onClick={(n) => onNumberClickHandler(n, colorToOperation.blue)}
+            >
+              {colorToOperation.blue(0, 0).str}
+            </CardDropDown>
+            <PlayingCard onClick={rotateColorToOperation}>↻</PlayingCard>
+            <PlayingCard
+              onClick={() => {
+                setTurnOps([
+                  ...turnOps,
+                  (index: number, points: number[]) => {
+                    return points.map((p_points: number) => {
+                      return -p_points;
+                    });
+                  },
+                ]);
+              }}
+            >
+              ⇄
+            </PlayingCard>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 items-center">
+          <Button
+            disabled={!canDoTurn}
             onClick={() => {
-              setTurnOps([
-                ...turnOps,
-                (index: number, points: number[]) => {
-                  return points.map((p_points: number) => {
-                    return -p_points;
-                  });
-                },
-              ]);
+              setTurnOps([]);
+              setCanDoTurn(false);
+              setAccumulator(previousAccumulator);
             }}
+            className="w-fit"
           >
-            ⇄
-          </PlayingCard>
+            Cancel
+          </Button>
+          <div className="flex flex-row gap-3 justify-center">
+            {createPlayerCards()}
+          </div>
         </div>
       </div>
-      <div className="flex flex-col gap-3 items-center">
-        <Button
-          disabled={!canDoTurn}
-          onClick={() => {
-            setTurnOps([]);
-            setCanDoTurn(false);
-            setAccumulator(previousAccumulator);
-          }}
-          className="w-fit"
-        >
-          Cancel
-        </Button>
-        <div className="flex flex-row gap-3 justify-center">
-          {createPlayerCards()}
-        </div>
-      </div>
-    </div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="fixed right-10 bottom-10">Rules</Button>
+        </DialogTrigger>
+        <DialogContent className="max-w-fit">
+          <DialogHeader>
+            <DialogTitle>Game Rules</DialogTitle>
+            <DialogDescription>
+              Read the official game rules here. It&apos;s recommended to follow
+              house rules if they exist.
+            </DialogDescription>
+          </DialogHeader>
+          <iframe
+            className="md:w-[720px] md:h-[600px]  rounded-sm"
+            src="https://docs.google.com/document/d/e/2PACX-1vTk_yXc3r2ARLyUIs3EhL8or5MHv_Siul3vvAk0uts331aKjmEbvp6XnU5ZFilDh_08bvHIGpQOQ8Dh/pub?embedded=true"
+          ></iframe>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
